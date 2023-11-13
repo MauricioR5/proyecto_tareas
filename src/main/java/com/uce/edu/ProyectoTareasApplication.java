32b18fdd44b96368e1618abe51a9c03947ebc004
Alexander Cacuango <amcacuangoa@uce.edu.ec>
2023-11-13 01:14:02 -0500

    Tarea 3

---
+++ src/main/java/com/uce/edu/ProyectoTareasApplication.java
@@ -0,0 +1,20 @@
+package com.uce.edu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTareasApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTareasApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Prueba de subida a Github en un proyecto nuevo");
+		
+	}
+
+}
